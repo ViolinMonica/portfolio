664e9aa225361cf57a198a21757bb1feffd50c0c
Violin <violinmonica190207@gmail.com>
2026-09-11 15:29:50 +0700

    refactor: change thumbnail field from URLField to ImageField

--- main/models.py
+++ main/models.py
@@ -15,7 +15,7 @@ class Experience(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     category = models.CharField(max_length=20, choices=EXPERIENCE_CHOICES, default='full-time')
-    thumbnail = models.URLField(blank=True, null=True)
+    thumbnail = models.ImageField(upload_to='thumbnails/', blank=True, null=True)
     started_at = models.DateTimeField(auto_now_add=True)
     ended_at = models.DateTimeField(blank=True, null=True)
     def __str__(self):
